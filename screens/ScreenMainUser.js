02754a20f2923d19bfe7e8e948c18d3d52bb24c9
DominusDrow <galacticovasquez@gmail.com>
2023-05-08 14:10:07 -0600

    init project structure

---
+++ screens/ScreenMainUser.js
@@ -0,0 +1,48 @@
+import { StyleSheet, View, ImageBackground } from 'react-native';
+import {DraggableButton}  from './components/Drag';
+import nave from '../assets/nave/nave.jpg';
+import { Stars } from '../components/background/Starts';
+import { Menu } from '../components/Menu';
+
+export default function ScreenMainUser({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={nave} style={styles.image}>
+        <Menu />
+        <View style={styles.startsContainer}>
+          <Stars />
+        </View>
+        <DraggableButton />
+      </ImageBackground>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  //style for the container view that should be a stage in in space
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  startsContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '50%',
+    height: '50%',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: '100%',
+    height: '100%',
+  },
+});
