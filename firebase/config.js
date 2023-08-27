b36f5893b988f147cef44f5b51f3854e00207b14
DominusDrow <galacticovasquez@gmail.com>
2023-08-26 22:33:22 -0600

    back to work

---
+++ firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+
+const provider = new GoogleAuthProvider()
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCS3tTZFVJSNAxHk_l8c1rh6_-WPZCS64Q',
+  authDomain: 'alientalk-app.firebaseapp.com',
+  projectId: 'alientalk-app',
+  storageBucket: 'alientalk-app.appspot.com',
+  messagingSenderId: '580270181432',
+  appId: '1:580270181432:web:c178baf39aad068b87eacd',
+  measurementId: 'G-KB8NT36JZV'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { auth }
